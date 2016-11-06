97c4c844e16e02935cd4c9a2881f28805dc6c41d
Fedor Fedoseev <fedorfdsv@gmail.com>
2016-11-06 20:34:25 +0100

    HelloUser added

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,8 @@
  * 
  */
 
+import java.util.Scanner;
+
 /**
  * @author Mohsen
  *
@@ -12,7 +14,11 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		HelloUser person = new HelloUser(scn.nextLine());
+		person.greetUser();
 	}
 
 }
